667cf3466ce057d1b379c030c0a17025521c76cd
nilamanandhar <nila.manandhar@grafioffshorenepal.com>
2022-03-25 10:22:44 +0545

    changes on design code

---
+++ src/component/SecondSwitch.js
@@ -0,0 +1,21 @@
+import React,{ useState} from 'react'
+
+export default function SecondSwitch() {
+    const [switchState, setswitchState] = useState(true);
+  return (
+    <div className={`${switchState ? 'second-switch-active' : ''} second-switch`}>
+        <svg className="svg" height="100%" width="100%" xmlns="http://www.w3.org/2000/svg">
+        <rect className={`${switchState ? "clickedShape " : "shape"}`}   height="60px" x="1px" y="1px" width="300px" rx="30" vector-effect="non-scaling-stroke" />
+        <rect className={`${!switchState ? "offClickedShape " : "offShape"}`}  height="60px" x="1px" y="1px" width="300px" rx="30" vector-effect="non-scaling-stroke" />
+        </svg>
+        <div onClick={()=>{setswitchState(true)}} className={`${switchState ? "second-on-active" : ""} second-on`} >
+        On
+        </div>
+        <div onClick={()=>{setswitchState(false)}} className={`${!switchState ? "second-off-active" : ""} second-off`}>
+        Off
+        </div>
+     </div>
+  )
+}
+
+
---
+++ src/component/ThirdSwitch.js
@@ -0,0 +1,19 @@
+import React,{ useState} from 'react'
+
+export default function ThirdSwitch() {
+    const [switchState, setswitchState] = useState(true);
+
+  return (
+    <div className={`${switchState ? 'third-switch-active' : ''} third-switch`}>
+        <svg className="svg" height="100%" width="100%" xmlns="http://www.w3.org/2000/svg">
+        <rect className={`${switchState ? "clickedShape " : "shape"}`}   height="60px" x="1px" y="1px" width="300px" rx="0" vector-effect="non-scaling-stroke" />
+        <rect className={`${!switchState ? "offClickedShape " : "offShape"}`}  height="60px" x="1px" y="1px" width="300px" rx="0" vector-effect="non-scaling-stroke" />
+        </svg>
+        <div onClick={() => setswitchState(true)} className={`${switchState ? "third-on-active" : ""} third-on`}  >On</div>
+        <div onClick={() => setswitchState(false)} className={`${!switchState ? "third-off-active" : ""} third-off`}> Off</div>
+    </div>
+  )
+}
+
+
+
